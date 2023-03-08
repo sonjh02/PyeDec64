a9f57606e150b7325652fc5066b7e66235bef288
Jaehyeon Son <sonjh02@gmail.com>
2023-03-09 00:44:44 +0900

    add sub push

--- pyedec64/parse_func.py
+++ pyedec64/parse_func.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 
-from .parse_opnd import parse_opnd
+from .parse_opnd import parse_opnd, Register, MemoryPtr
 from .parse_inst import parse_inst
 from .image_stream import ImageStream
 
@@ -37,14 +37,36 @@ def parse_func(image: ImageStream):
 
             inst_graph[addr] = inst = Inst(*inst_base, [], [], [], [])
             rip_addr = addr + len(code)
+            rsp_opnd = Register('rsp', 8)
+            of_opnd = StateFlag('OF')
+            sf_opnd = StateFlag('SF')
+            zf_opnd = StateFlag('ZF')
+            af_opnd = StateFlag('AF')
+            pf_opnd = StateFlag('PF')
+            cf_opnd = StateFlag('CF')
 
             if mnemonic == 'mov':
                 dst, src = op_str.split(', ')
                 inst.depends.append(parse_opnd(src, rip_addr))
                 inst.affects.append(parse_opnd(dst, rip_addr))
                 inst.links.append(rip_addr)
-            # elif mnemonic == 'push':
-            #     pass
+            elif mnemonic == 'push':
+                src_opnd = parse_opnd(op_str, rip_addr)
+                size = src_opnd.size
+                inst.depends.append(src_opnd)
+                inst.affects.append(MemoryPtr('rsp + %d' % size, size))
+                inst.affects.append(rsp_opnd)
+            elif mnemonic == 'sub' or mnemonic == 'add':
+                dst, src = op_str.split(', ')
+                inst.depends.append(parse_opnd(src, rip_addr))
+                inst.affects.append(parse_opnd(dst, rip_addr))
+                inst.affects.append(of_opnd)
+                inst.affects.append(sf_opnd)
+                inst.affects.append(zf_opnd)
+                inst.affects.append(af_opnd)
+                inst.affects.append(pf_opnd)
+                inst.affects.append(cf_opnd)
+                inst.links.append(rip_addr)
             else:
                 raise NotImplementedError(mnemonic)
 
--- pyedec64/parse_opnd.py
+++ pyedec64/parse_opnd.py
@@ -1,8 +1,42 @@
 from collections import namedtuple
 
 
-Register = namedtuple("Register", "name")
+Register = namedtuple("Register", ["name", "size"])
 MemoryPtr = namedtuple("MemoryPtr", ["addr", "size"])
+Immediate = namedtuple("Immediate", ["value"])
+
+
+register_sizes = [
+    (
+        8,
+        set(
+            ['r' + v for v in ['ax', 'bx', 'cx', 'dx', 'si', 'di', 'bp', 'sp']]
+            + ['r%d' % v for v in range(8, 16)]
+        ),
+    ),
+    (
+        4,
+        set(
+            ['e' + v for v in ['ax', 'bx', 'cx', 'dx', 'si', 'di', 'bp', 'sp']]
+            + ['r%dd' % v for v in range(8, 16)]
+        ),
+    ),
+    (
+        2,
+        set(
+            ['ax', 'bx', 'cx', 'dx', 'si', 'di', 'bp', 'sp']
+            + ['r%dw' % v for v in range(8, 16)]
+        ),
+    ),
+    (
+        1,
+        set(
+            [v + 'l' for v in 'abcd']
+            + ['r%db' % v for v in range(8, 16)]
+            + ['sil', 'dil', 'bpl', 'spl']
+        ),
+    ),
+]
 
 
 memory_sizes = [
@@ -24,4 +58,12 @@ def parse_opnd(target_str: str, rip_addr: int):
             if target_str.startswith('rip - '):
                 return MemoryPtr(rip_addr - int(target_str[6:]), size)
             return MemoryPtr(target_str, size)
-    return Register(target_str)
+
+    for size, register_set in register_sizes:
+        if target_str in register_set:
+            return Register(target_str, size)
+
+    if target_str.startswith('0x'):
+        return Immediate(target_str)
+
+    raise NotImplementedError(target_str)
